5f7890ddb79379dd845a60cd73896c99f58be194
unknown <rjohnquia@gmail.com>
2023-06-07 08:49:00 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+
+/*######## INTRODUCTION ##########*/
+//echo ' <h1> hello world  </h1>';
+//print
+//print_r([1, 2, 3]);
+//echo 'echo in php <br/>'
+//installation
+//xammp
+
+/*######## VARIABLES ##########*/
+// $variable;
+// $$variables;
+// $password;
+// $username;
+// $_username;
+// $Admin;
+//  echo " hello {$admin}";
+//  echo "hello". $admin;
+
+/*######## DATA TYPE ##########*/
+// $string = 'this is a string' ;
+// $integer = 'this is an int'.  123;
+// $float = 1.5
+// $Booleans = true
+// $array = array(1,2,"banan", "apple");
+    //$array[1]
+    //var_dump($array[1]);
+// $object = {"password":"123", "username":"john"}
+// $resource =
+// $null = 
+
+// $numarray1 = array('hello', "he");
+// $numarray2 = array("hell", "lo");
+
+// $numarray1 = array_merge($numarray1 , $numarray2);
+
+// var_dump($numarray1) ;
+// echo $numarray1;
+
+
+// echo $string ; 
+// echo $int;
+
+
+/*######## ARITHMETIC OPERATORS ##########*/
+
+//+ - * / %
+
+
+/*######## ASSIGNMENT OPERATORS ##########*/
+
+// += Add and assign
+// -= Subtract and assign
+// *= Multiply and assign
+// /= Divide and assign
+// .= Concatenate and assign
+
+/*######## COMPARISON OPERATORS ##########*/
+
+
+
+ 
+
+
+
+
+
+
+
